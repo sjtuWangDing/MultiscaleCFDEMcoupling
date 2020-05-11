8d50414a23bb4d7e873278f2c035cf0ce67d6fd4
sjtuWangDing <2941726668@qq.com>
2020-05-11 14:57:07 +0800

    doc(): fix .gitignore file

--- test_mix/01_octave_one_particle_sediment_in_silicon_oil/postproc.m
+++ test_mix/01_octave_one_particle_sediment_in_silicon_oil/postproc.m
@@ -1,24 +1,23 @@
 % postproc.m: loads the data necessary and saves the graphs
+% (compared to the reults by glowinski)
 
 clear;
 clc;
 close all;
 
 % read data from simulation
-vel_IB = load('../oneParticlesSediment_silicon_oil_cfdemSolverIB/DEM/post/velocity_particle_1.txt');
-vel_mix = load('../oneParticlesSediment_silicon_oil_cfdemSolverMix/DEM/post/velocity_particle_1.txt');
+particle_vel_1 = load('../../DEM/post/velocity_particle_1.txt');
 linienstaerke = 1;
 MarkerGroesse = 4;
 
 figure(2)
-h = plot(vel_IB(:, 1), vel_IB(:, 4), '*',
-         vel_mix(:, 1), vel_mix(:, 4), '+');
+h = plot(particle_vel_1(:, 1), particle_vel_1(:, 4), '*');
 set(h,'LineWidth', linienstaerke, 'MarkerSize', MarkerGroesse);
 set(gca, 'FontSize', 12)
 axis([0.0 1.4 -0.14 0.0])
 xlabel('time (s)')
 ylabel('z-veloctiy (m/s)')
-title('Comparison of the settling velocity of two solvers', 'FontSize', 12)
-legend('cfdemSolverIB', 'cfdemSolverMix')
+title('Comparison of the settling velocity of two particles', 'FontSize', 12)
+legend('following particle', 'leading particle')
 set(gca, 'FontSize', 12)
-print('settling_direction_velocity.png')
+print('vel_y_two_particles.png')
