bfb56736c9e1736ecf3e829cc865687d441777ff
sjtuWangDing <2941726668@qq.com>
2020-06-05 21:02:55 +0800

    xxx

--- test_mix/oneParticlesSediment_silicon_oil_cfdemSolverMix/CFD/octave/postproc.m
+++ test_mix/oneParticlesSediment_silicon_oil_cfdemSolverMix/CFD/octave/postproc.m
@@ -6,44 +6,18 @@ clc;
 close all;
 
 % read data from simulation
-particle_pos_1 = load('../../DEM/post/position_particle_1.txt');
-particle_pos_2 = load('../../DEM/post/position_particle_2.txt');
-
-linienstaerke = 1;
-MarkerGroesse = 4;
-
-figure(1)
-offset = particle_pos_1(1, 4);
-h = plot(particle_pos_1(:, 1), particle_pos_1(:, 4), '*',
-         particle_pos_2(:, 1), particle_pos_2(:, 4), '+');
-set(h,'LineWidth', linienstaerke, 'MarkerSize', MarkerGroesse);
-set(gca, 'FontSize', 12)
-axis([0.0 0.7 0.0 0.04])
-xlabel('time (s)')
-ylabel('position (m)')
-title('Comparison of the y-position of two particles', 'FontSize', 12)
-legend('following particle', 'leading particle')
-set(gca, 'FontSize', 12)
-print('pos_y_two_particles.png')
-
-clear;
-
-% read data from simulation
-particle_vel_1 = load('../../DEM/post/velocity_particle_1.txt');
-particle_vel_2 = load('../../DEM/post/velocity_particle_2.txt');
-
+vel = load('../../DEM/post/velocity_particle_1.txt');
 linienstaerke = 1;
 MarkerGroesse = 4;
 
 figure(2)
-h = plot(particle_vel_1(:, 1), particle_vel_1(:, 4), '*',
-         particle_vel_2(:, 1), particle_vel_2(:, 4), '+');
+h = plot(vel(:, 1), vel(:, 4), '*');
 set(h,'LineWidth', linienstaerke, 'MarkerSize', MarkerGroesse);
 set(gca, 'FontSize', 12)
-axis([0.0 0.7 -0.075 0.0])
+axis([0.0 1.4 -0.14 0.0])
 xlabel('time (s)')
 ylabel('z-veloctiy (m/s)')
-title('Comparison of the settling velocity of two particles', 'FontSize', 12)
-legend('following particle', 'leading particle')
+title('Settling velocity using cfdemSolverMix', 'FontSize', 12)
+legend('cfdemSolverMix')
 set(gca, 'FontSize', 12)
-print('vel_y_two_particles.png')
+print('settling_direction_velocity.png')
